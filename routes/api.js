3b55b3bcd09f4ad02b049ad08c7507cc487d33ef
Nino chan <81684610+Nino-chan02@users.noreply.github.com>
2021-05-11 02:12:01 +0700

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -112,7 +112,7 @@ loghandler = {
         status: false,
         creator: `${creator}`,
         code: 406,
-        message: 'apikey invalid, gak punya apikey? chat gw aja yaaa di wa.me/6283898698875 , key nya gratis kok gan, jadi santuyy ajaa'
+        message: 'Apikey? chat wa.me/6288286421519'
     },
     invalidlink: {
         status: false,
